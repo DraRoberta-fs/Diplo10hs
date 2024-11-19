e9ec150faf18a776ca51c8da1a0a2cb74aeff50d
Agustina Onorati <agon.baires@gmail.com>
2024-11-19 15:24:14 +0000

    errores react

---
+++ transportes/frontend/new-transportes/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componentes/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <header>hola</header>
+   
+    </div>
+  );
+}
+
+export default App;
